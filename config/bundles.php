6702365b7aa42031a88667254e9ab49bfb1fadc6
Denis Pacha <cool.den0@yandex.ru>
2023-09-27 02:33:55 +0300

    admin dashboard

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
